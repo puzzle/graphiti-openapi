617a4e34747ea7c008ada10e255d822636adee3e
Daniel Illi <daniel@illi.zone>
2024-04-08 11:43:46 +0200

    Remove obsolete routes.rb

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Graphiti::OpenApi::Engine.routes.draw do
-  resources :specifications, only: :index, defaults: {format: :html}
-
-  get "openapi.:format" => "specifications#index", defaults: {format: :json}, as: :openapi
-  root to: "specifications#index", defaults: {format: :html}
-end
