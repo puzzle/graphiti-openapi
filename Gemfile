750794bd7edb6a8c69f2357f645aa06fb292d85c
Daniel Illi <daniel@illi.zone>
2025-07-07 16:50:04 +0200

    Relax dep version constraint for graphiti gem, cleanup cruft

--- Gemfile
+++ Gemfile
@@ -1,11 +1,4 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in graphiti-openapi.gemspec
 gemspec
-
-gem "graphiti"
-
-group :development, :test do
-  gem "rspec"
-  gem "rspec-its"
-end
--- lib/templates/installer.rb
+++ lib/templates/installer.rb
@@ -1,20 +1,4 @@
-root = File.expand_path("../..", __dir__)
-packs = File.directory?("app/assets/packs") ? "app/assets/packs" : "app/javascripts/packs"
+root = File.expand_path('../..', __dir__)
 
-say "Copying api.js to #{packs}/"
-copy_file "#{root}/app/assets/packs/api.js", "#{packs}/api.js"
-
-say "Copying openapi.yml to config/"
-copy_file "#{root}/config/openapi.yml", "config/openapi.yml"
-
-say "Ignoring openapi specifications"
-File.write ".gitignore", "" unless File.file?(".gitignore")
-append_to_file ".gitignore" do
-  <<~IGNORE
-    public/api/v1/openapi.json
-    public/api/v1/openapi.yaml
-  IGNORE
-end
-
-say "Installing JavaScript dependencies"
-run "yarn add swagger-ui"
+say 'Copying openapi.yml to config/'
+copy_file "#{root}/config/openapi.yml", 'config/openapi.yml'
